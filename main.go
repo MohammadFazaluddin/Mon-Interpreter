6ab77a5cbed8ddba918a7e31eab9cc2bf2d8fc47
MohammadFazaluddin <fazaluddin2806@gmail.com>
2024-07-08 13:01:15 +0530

    REPL added with only tokanization

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"Mon/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Mon Programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in command!\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
